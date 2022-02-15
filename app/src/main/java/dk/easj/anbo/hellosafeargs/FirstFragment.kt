0f7c82d998d5597e6a9a436e6fdd08021f128077
Anders Børjesson <anbo@zealand.dk>
2022-02-15 08:34:26 +0100

    gradle update

--- app/src/main/java/dk/easj/anbo/hellosafeargs/FirstFragment.kt
+++ app/src/main/java/dk/easj/anbo/hellosafeargs/FirstFragment.kt
@@ -8,15 +8,8 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import dk.easj.anbo.hellosafeargs.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
-
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
--- app/src/main/java/dk/easj/anbo/hellosafeargs/SecondFragment.kt
+++ app/src/main/java/dk/easj/anbo/hellosafeargs/SecondFragment.kt
@@ -1,23 +1,17 @@
 package dk.easj.anbo.hellosafeargs
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import dk.easj.anbo.hellosafeargs.databinding.FragmentSecondBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SecondFragment : Fragment() {
-
     private var _binding: FragmentSecondBinding? = null
     private val args: SecondFragmentArgs by navArgs() // update both gradle files
-
-    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,7 +20,6 @@ class SecondFragment : Fragment() {
     ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,7 +27,8 @@ class SecondFragment : Fragment() {
         binding.textviewSecond.text = "Hello " + args.personname
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            //findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().popBackStack()
         }
     }
 
